4ff7a3169369de52acfd965dcaf8abd493c67612
Johnny-Tech-Oche <oche.j007@gmail.com>
2024-07-08 12:11:00 +0100

    Add files via upload

---
+++ Choice.py
@@ -0,0 +1,11 @@
+is_hot = False
+is_cold = True
+if is_hot:
+    print(" it's a hot day ")
+    print(" drink ice-cold water ")
+elif is_cold:
+    print(" it's a cold day ")
+    print(" wear warm clothes ")
+else:
+    print(" enjoy the weather ")
+    print(" it's a lovely day ")
